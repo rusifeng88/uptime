86aa71cb253b5ebd2f979e7ea5c2313ed03b8b05
D <rusifeng88@gmail.com>
2025-10-03 10:38:09 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,6 +1,6 @@
 const pageConfig = {
   // Title for your status page
-  title: "数字套利 •「AM科技」's Status Page",
+  title: "Devin's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://amclubss.com', label: '博客', highlight: true },
